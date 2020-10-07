36dfa57144993951c1b6c0e619fd3ae03ceacd18
indahmawarni <indahmh74@gmail.com>
2020-10-07 13:51:00 +0700

    pertemuan1

---
+++ akumul.php
@@ -0,0 +1,11 @@
+<form method="GET">
+    <input type="number" name="bil">
+    <input type="submit" value="jumlahkan">
+    <?php
+        if (isset($_GET['bil'])){
+            $data = new array();
+            array_push($data,)
+        }
+    ?>
+
+</form>
